a3fc4c691c75f07d683a46afbd6cab005f64150e
Vasu Kalariya <vasukalariya2@gmail.com>
2020-09-14 14:29:59 +0530

    Complete Linked List

---
+++ Linked List/remove_loop_in_linked_list.cpp
@@ -0,0 +1,147 @@
+// { Driver Code Starts
+// driver code
+
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void loopHere(Node* head, Node* tail, int position)
+{
+    if(position==0) return;
+    
+    Node* walk = head;
+    for(int i=1; i<position; i++)
+        walk = walk->next;
+    tail->next = walk;
+}
+
+bool isLoop(Node* head)
+{
+    if(!head) return false;
+    
+    Node* fast = head->next;
+    Node* slow = head;
+    
+    while( fast != slow)
+    {
+        if( !fast || !fast->next ) return false;
+        fast=fast->next->next;
+        slow=slow->next;
+    }
+    
+    return true;
+}
+
+int length(Node* head)
+{
+    int ret = 0;
+    while(head)
+    {
+        ret++;
+        head = head->next;
+    }
+    return ret;
+}
+
+void removeLoop(Node* head);
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, num;
+        cin>>n;
+        
+        Node *head, *tail;
+        cin>> num;
+        head = tail = new Node(num);
+        
+        for(int i=0 ; i<n-1 ; i++)
+        {
+            cin>> num;
+            tail->next = new Node(num);
+            tail = tail->next;
+        }
+        
+        int pos;
+        cin>> pos;
+        loopHere(head,tail,pos);
+        
+        removeLoop(head);
+        
+        if( isLoop(head) || length(head)!=n )
+            cout<<"0\n";
+        else
+            cout<<"1\n";
+    }
+	return 0;
+}
+// } Driver Code Ends
+
+
+/*
+structure of linked list node:
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+*/
+
+void removeLoop(Node* head)
+{
+    // code here
+    // just remove the loop without losing any nodes
+    if(head == NULL || head->next == NULL) return;
+    
+    Node* slow = head->next;
+    Node* fast = head->next->next;
+    
+    while(fast && slow != fast){
+        
+        slow = slow->next;
+        if(fast->next == NULL) fast = NULL;
+        else fast = fast->next->next;
+        
+    }
+    if(fast == NULL) return;
+    
+    slow = head;
+    
+    if(fast == head){
+        while(slow->next != head) slow = slow->next;
+        
+        slow->next = NULL;
+        return;
+    }
+    
+    while(slow->next != fast->next){
+        slow = slow->next;
+        fast = fast->next;
+    }
+    
+    fast->next = NULL;
+    
+    return;
+}
