deec0b8f457ad094a0c5a306624882e50ed81f2f
Vasu Kalariya <vasukalariya2@gmail.com>
2020-09-19 13:10:51 +0530

    Complete Linked List

---
+++ Linked List/implement_queue_using_linked_list.cpp
@@ -0,0 +1,104 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct QueueNode
+{
+    int data;
+    QueueNode *next;
+    QueueNode(int a)
+    {
+        data = a;
+        next = NULL;
+    }
+};
+
+struct MyQueue {
+    QueueNode *front;
+    QueueNode *rear;
+    void push(int);
+    int pop();
+    MyQueue() {front = rear = NULL;}
+};
+
+
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        MyQueue *sq = new MyQueue();
+
+        int Q;
+        cin>>Q;
+        while(Q--){
+        int QueryType=0;
+        cin>>QueryType;
+        if(QueryType==1)
+        {
+            int a;
+            cin>>a;
+            sq->push(a);
+        }else if(QueryType==2){
+            cout<<sq->pop()<<" ";
+
+        }
+        }
+        cout<<endl;
+    }
+    }
+// } Driver Code Ends
+
+
+/* Structure of a node in Queue
+struct QueueNode
+{
+    int data;
+    QueueNode *next;
+    QueueNode(int a)
+    {
+        data = a;
+        next = NULL;
+    }
+};
+
+And structure of MyQueue
+struct MyQueue {
+    QueueNode *front;
+    QueueNode *rear;
+    void push(int);
+    int pop();
+    MyQueue() {front = rear = NULL;}
+}; */
+
+/* The method push to push element into the queue*/
+void MyQueue:: push(int x)
+{
+    QueueNode* node = new QueueNode(x);
+    if(front == NULL){
+        front = node;
+        rear = node;
+    }
+    else{
+        rear->next = node;
+        rear = node;
+    }
+        // Your Code
+}
+
+/*The method pop which return the element
+  poped out of the queue*/
+int MyQueue :: pop()
+{
+    if(front == NULL) return -1;
+    else{
+        int d = front->data;
+        front = front->next;
+        
+        return d;
+    }
+        // Your Code       
+}
+
---
+++ Linked List/implement_stack_using_linked_list.cpp
@@ -0,0 +1,101 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct StackNode {
+    int data;
+    StackNode *next;
+    StackNode(int a) {
+        data = a;
+        next = NULL;
+    }
+};
+
+class MyStack {
+  private:
+    StackNode *top;
+
+  public:
+    void push(int);
+    int pop();
+    MyStack() { top = NULL; }
+};
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        MyStack *sq = new MyStack();
+
+        int Q;
+        cin >> Q;
+        while (Q--) {
+            int QueryType = 0;
+            cin >> QueryType;
+            if (QueryType == 1) {
+                int a;
+                cin >> a;
+                sq->push(a);
+            } else if (QueryType == 2) {
+                cout << sq->pop() << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+// } Driver Code Ends
+
+
+/*
+The structure of the node of the stack is
+struct StackNode
+{
+    int data;
+    StackNode *next;
+    StackNode(int a)
+    {
+        data = a;
+        next = NULL;
+    }
+};
+
+// And this is structure of MyStack
+class MyStack {
+private:
+StackNode *top;
+public :
+    void push(int);
+    int pop();
+    MyStack()
+    {
+        top = NULL;
+    }
+};
+
+/* The method push to push element
+   into the stack */
+void MyStack ::push(int x) {
+    StackNode* sn = new StackNode(x);
+    if(top == NULL){
+        top = sn;
+    }
+    else{
+        sn->next = top;
+        top = sn;
+    }
+    // Your Code
+}
+
+/* The method pop which return the element
+  poped out of the stack*/
+int MyStack ::pop() {
+    
+    if(top == NULL) return -1;
+    else{
+        int d = top->data;
+        top = top->next;
+        
+        return d;
+    }
+    // Your Code
+}
